144f3032758a2c1626fd610b5bc2ec5c0c71f8d9
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2024-11-03 16:10:01 +0100

    implement axios to download image from API

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import axios from "axios";
+import fs from "node:fs";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://covers.openlibrary.org/b/isbn/0385472579-S.jpg",
+      { responseType: "arraybuffer" }
+    );
+    console.log(response.data);
+    fs.writeFile("test.jpg", response.data, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved.");
+    });
+  } catch (error) {
+    console.error("Failed to make request: ", error.message);
+  }
+
   res.render("index.ejs");
 });
 
