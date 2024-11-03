407276d9ef0f5f6bd1cf671a1936985e3ad5040e
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2024-11-03 10:12:18 +0100

    dummy ejs running

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.listen(port, () => {
   console.log(`Hi, I am Server. I am running on port ${port}.`);
